bbee1c2678658044b32f69a33e4540c73cf05df5
kapildadhich075 <kapildadhich075@gmail.com>
2021-04-25 18:25:04 +0530

    rectifying the error.

--- lib/Screens/signup_page.dart
+++ lib/Screens/signup_page.dart
@@ -82,7 +82,7 @@ class _SignUpState extends State<SignUp> {
                     showSpinner = true;
                   });
                   try {
-                    final user = await _auth.signInWithEmailAndPassword(email: email, password: password);
+                    final user = await _auth.createUserWithEmailAndPassword(email: email, password: password);
 
                     if (user != null) {
                       Navigator.pushNamed(context, MainScreen.page);
